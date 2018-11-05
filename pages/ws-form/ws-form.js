d38809c72bf56892f2b81648103438334c6f0471
jiang.wenqi <jiang.wenqi@navercorp.com>
2018-11-05 16:59:04 +0800

    #

--- pages/ws-form/ws-form.js
+++ pages/ws-form/ws-form.js
@@ -3,7 +3,7 @@ const app = getApp()
 
 Page({
   data: {
-
+    files: []
   },
   //事件处理函数
   bindViewTap: function () {
@@ -11,7 +11,26 @@ Page({
       url: '../register/register?id=' + 123
     })
   },
-  onLoad: function () {
-
+  onLoad: function (routeParams) {
+    console.log('routeParams->', routeParams)
+  },
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+      }
+    })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
   }
 })
