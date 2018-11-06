96cdb5b220c8920a12c762aa2dc977ea80870067
jwq403926021 <jwq403926021@163.com>
2018-11-06 11:14:48 +0800

    #

--- pages/register/register.js
+++ pages/register/register.js
@@ -8,10 +8,30 @@ Page({
     companyCategory: 0,
     companyName: 0,
     companyCategoryList: ['Type 1', 'Type 2', 'Type 3'],
-    companyNameList: ['Name 1', 'Name 2', 'Name 3']
+    companyNameList: ['Name 1', 'Name 2', 'Name 3'],
+    regionData: {
+      "abc": {
+        "eee": ["1","2"]
+      }
+    },
+    regionPickerFlag: false
   },
   onLoad: function (routeParams) {
     console.log('routeParams->', routeParams)
+
+  },
+  onChange3 (e) {
+    console.log("onChange3", e)
+  },
+  cancelRegionPicker () {
+    this.setData({
+      regionPickerFlag: false
+    })
+  },
+  openRegionPicker () {
+    this.setData({
+      regionPickerFlag: true
+    })
   },
   bindRegionChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
