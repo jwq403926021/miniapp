2b65166adfc8133b564f9b22027d120362edb620
jwq403926021 <jwq403926021@163.com>
2019-01-01 19:40:29 +0800

    #

--- pages/cx-form/cx-form.js
+++ pages/cx-form/cx-form.js
@@ -1,9 +1,26 @@
 //获取应用实例
+import util from "../../utils/util";
+
 const app = getApp()
 
 Page({
   data: {
-    
+    id: null,
+    role: 1,
+    show: false,
+    areaList: {},
+    region: '',
+    regionLabel: '',
+    taskData: {
+      autoInsuranceName: '',
+      autoInsuranceMobile: '',
+      plateNumber: '',
+      type: '1',
+      areaCode: '',
+      cityCode: '',
+      provinceCode: '',
+      remark: ''
+    }
   },
   //事件处理函数
   bindViewTap: function() {
@@ -11,7 +28,190 @@ Page({
       url: '../register/register?id='+123
     })
   },
-  onLoad: function () {
-    
+  onLoad: function (routeParams ) {
+    console.log('车险 工单号：->', routeParams)
+    console.log('当前用户信息->', app.globalData.currentRegisterInfo)
+    this.initArea()
+    if (routeParams && routeParams.id) {
+      this.setData({
+        id: routeParams.id,
+        role: app.globalData.currentRegisterInfo.role// 1 查勘员 | 12 施工人员 | 6 公司市级负责人 | 11 合作商市级负责人 | TODO::: app.globalData.currentRegisterInfo.role
+      })
+      this.initDataById(routeParams.id)
+    }
+  },
+  initDataById (id) {
+    let _this = this
+    util.request({
+      path: '/app/damage/damageDetail',
+      method: 'GET',
+      data: {
+        damageId: id
+      }
+    }, function (err, res) {
+      let data = res.data
+      _this.sourceData = data
+      _this.sourceImage = res.Image
+      // let informationImageFiles = []
+      // let liveImageFiles = []
+      // let workLiveImageFiles = []
+      // let damageImageFiles = []
+      // let authorityImageFiles = []
+      // let caleImageFiles = []
+      // _this.sourceImage.forEach(item => {
+      //   switch (item.type) {
+      //     case 1:
+      //       informationImageFiles.push(`https://aplusprice.xyz/file/${item.path}`)
+      //       break
+      //     case 2:
+      //       liveImageFiles.push(`https://aplusprice.xyz/file/${item.path}`)
+      //       break
+      //     case 3:
+      //       workLiveImageFiles.push(`https://aplusprice.xyz/file/${item.path}`)
+      //       break
+      //     case 4:
+      //       damageImageFiles.push(`https://aplusprice.xyz/file/${item.path}`)
+      //       break
+      //     case 5:
+      //       authorityImageFiles.push(`https://aplusprice.xyz/file/${item.path}`)
+      //       break
+      //     case 7:
+      //       caleImageFiles.push(`https://aplusprice.xyz/file/${item.path}`)
+      //       break
+      //   }
+      // })
+      _this.setData({
+        'taskData.status': data.status,
+        'taskData.area': data.area,
+        'taskData.insuranceType': data.insuranceType,
+        'taskData.damagedUser': data.damagedUser,
+        'taskData.damagedPhone': data.damagedPhone || '',
+        'taskData.customerUser': data.customerUser,
+        'taskData.customerPhone': data.customerPhone,
+        'taskData.plateNumber': data.plateNumber,
+        'taskData.information': data.information,
+        'taskData.live': data.live,
+        "taskData.surveyUser": data.surveyUser,
+        "taskData.surveyPhone": data.surveyPhone,
+        "taskData.workerUser": data.workerUser,
+        "taskData.workerPhone": data.workerPhone,
+        "taskData.workType": data.workType,
+        "taskData.budgetPreliminary": data.budgetPreliminary,
+        'taskData.handlingType': data.handlingType,
+        'taskData.deposit': data.deposit,
+        'taskData.trasactionId': data.trasactionId,
+        'taskData.offer': data.offer,
+        'taskData.bidder': data.bidder,
+        'taskData.offerRemark': data.offerRemark
+      })
+      _this.getRegionLabel()
+    })
+  },
+  initArea () {
+    let _this = this
+    _this.setData({
+      region: app.globalData.currentRegisterInfo.townCode,
+      'taskData.area': app.globalData.currentRegisterInfo.townCode,
+      'taskData.areaCode': app.globalData.currentRegisterInfo.townCode,
+      'taskData.cityCode': app.globalData.currentRegisterInfo.cityCode,
+      'taskData.provinceCode': app.globalData.currentRegisterInfo.provinceCode
+    })
+    util.request({
+      path: '/sys/area/list',
+      method: 'GET'
+    }, function (err, res) {
+      _this.setData({
+        areaList: res.DATA.DATA
+      })
+      _this.getRegionLabel()
+    })
+  },
+  getRegionLabel () {
+    let arr = []
+    if (this.data.region && this.data.areaList.hasOwnProperty('province_list')) {
+      let provinceCode = this.data.region.slice(0,2) + '0000'
+      let cityCode = this.data.region.slice(0,4) + '00'
+      let townCode = this.data.region
+      arr.push(this.data.areaList['province_list'][provinceCode])
+      arr.push(this.data.areaList['city_list'][cityCode])
+      arr.push(this.data.areaList['county_list'][townCode])
+    }
+    this.setData({
+      regionLabel: arr.length ? arr.join(',') : ''
+    })
+  },
+  openLocation() {
+    this.setData({
+      show: !this.show
+    })
+  },
+  onConfirm(data) {
+    let strArr = []
+    data.detail.values.forEach(item => {
+      strArr.push(item.name)
+    })
+
+    this.setData({
+      show: false,
+      region: data.detail.values[2].code,
+      regionLabel: strArr.join(','),
+      'taskData.areaCode': data.detail.values[2].code,
+      'taskData.cityCode': data.detail.values[1].code,
+      'taskData.provinceCode': data.detail.values[0].code,
+    })
+  },
+  onCancel() {
+    this.setData({
+      show: false
+    })
+  },
+  inputgetName(e) {
+    let name = e.currentTarget.dataset.name;
+    let nameMap = {}
+    if (name.indexOf('.')) {
+      let nameList = name.split('.')
+      if (this.data[nameList[0]]) {
+        nameMap[nameList[0]] = this.data[nameList[0]]
+      } else {
+        nameMap[nameList[0]] = {}
+      }
+      nameMap[nameList[0]][nameList[1]] = e.detail.value
+    } else {
+      nameMap[name] = e.detail.value
+    }
+    this.setData(nameMap)
+  },
+  dialPhone (e) {
+    let phone = e.currentTarget.dataset.phone;
+    wx.makePhoneCall({
+      phoneNumber: phone
+    })
+  },
+  onTypeChange (event) {
+    this.setData({
+      'taskData.type': event.detail
+    });
+  },
+  isLicenseNo(str){
+    str = str.replace(/\s+/g,"")
+    if (str) {
+      let flag = /(^[\u4E00-\u9FA5]{1}[A-Za-z0-9]{6}$)|(^[A-Za-z]{2}[A-Za-z0-9]{2}[A-Za-z0-9\u4E00-\u9FA5]{1}[A-Za-z0-9]{4}$)|(^[\u4E00-\u9FA5]{1}[A-Za-z0-9]{5}[挂学警军港澳]{1}$)|(^[A-Za-z]{2}[0-9]{5}$)|(^(08|38){1}[A-Za-z0-9]{4}[A-Za-z0-9挂学警军港澳]{1}$)/.test(str);
+      if (!flag) {
+        wx.showToast({
+          title: '车牌号不正确',
+          icon: 'none',
+          duration: 2000
+        })
+        return false
+      }
+    } else {
+      wx.showToast({
+        title: '车牌号不能为空',
+        icon: 'none',
+        duration: 2000
+      })
+      return false
+    }
+    return true
   }
 })
