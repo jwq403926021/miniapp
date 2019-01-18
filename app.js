581dd1cb4e2dfaddda8ef50a570aa2106c5d09d6
jwq403926021 <jwq403926021@163.com>
2019-01-18 08:56:05 +0800

    # fix

--- app.js
+++ app.js
@@ -13,6 +13,38 @@ App({
   },
   login () {
     let _this = this
+
+    if (wx.canIUse('getUpdateManager')) { // 基础库 1.9.90 开始支持，低版本需做兼容处理
+      const updateManager = wx.getUpdateManager();
+      updateManager.onCheckForUpdate(function(result) {
+        if (result.hasUpdate) { // 有新版本
+          updateManager.onUpdateReady(function() { // 新的版本已经下载好
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经下载好，请重启应用。',
+              success: function(result) {
+                if (result.confirm) { // 点击确定，调用 applyUpdate 应用新版本并重启
+                  updateManager.applyUpdate();
+                }
+              }
+            });
+          });
+          updateManager.onUpdateFailed(function() { // 新的版本下载失败
+            wx.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+            });
+          });
+        }
+      });
+    }
+    else { // 有更新肯定要用户使用新版本，对不支持的低版本客户端提示
+      wx.showModal({
+        title: '温馨提示',
+        content: '当前微信版本过低，无法使用该应用，请升级到最新微信版本后重试。'
+      });
+    }
+
     wx.login({
       success: function (res) {
         if (res.code) {
