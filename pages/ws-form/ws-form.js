c999fb7086db4bcdb4ad10a7080a7e101bbffb27
jwq403926021 <jwq403926021@163.com>
2019-10-21 08:55:29 +0800

    # 车牌号 正则

--- pages/ws-form/ws-form.js
+++ pages/ws-form/ws-form.js
@@ -179,6 +179,7 @@ Page({
     return true
   },
   isLicenseNo(str){
+    str = str.toUpperCase()
     str = str.replace(/\s+/g,"")
     if (str) {
       let flag = /^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$/.test(str);
