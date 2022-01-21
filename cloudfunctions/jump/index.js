f9dddeea63de99417f82571ee094d1aab9e96e65
jiangwenqi <jwq403926021@163.com>
2022-01-21 11:25:09 +0800

    #

--- cloudfunctions/jump/index.js
+++ cloudfunctions/jump/index.js
@@ -6,20 +6,19 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-
   switch (event.action) {
     case 'getUrlScheme': {
-      return getUrlScheme(event.query)
+      return getUrlScheme(event.id)
     }
   }
-  return 'action not found'
+  return event
 }
 
-async function getUrlScheme(query) {
+async function getUrlScheme(id) {
   return cloud.openapi.urlscheme.generate({
     jumpWxa: {
-      path: '/pages/index/index', // <!-- replace -->
-      query: query,
+      path: '/pages/new-ws-form/new-ws-form', // <!-- replace -->
+      query: `id=${id}`
     },
     // 如果想不过期则置为 false，并可以存到数据库
     isExpire: false,
