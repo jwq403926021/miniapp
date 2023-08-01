975216ba5f084a925d52a295b846817744012e5a
jiangwenqi <jwq403926021@163.com>
2023-08-01 11:19:06 +0800

    #沪家保

--- pages/hujiabao-form/hujiabao-form.js
+++ pages/hujiabao-form/hujiabao-form.js
@@ -23,6 +23,7 @@ Page({
       '6': '核损通过',
       '7': '已关闭',
     },
+    investigatorImageFiles: [],
     delayReportReason: { index: '', value: '', label: '' },
     delayReportReasonList: Object.values(MetaData.delayReportReason),
   },
@@ -43,5 +44,147 @@ Page({
   },
   async onLoad (routeParams) {
 
-  }
+  },
+  chooseImage (e) {
+    let key = e.currentTarget.dataset.name
+    let that = this;
+    app.globalData.isIgnoreRefresh = true
+    wx.chooseImage({
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        res.tempFilePaths.forEach((item, index) => {
+          wx.compressImage({
+            src: item,
+            quality: res.tempFiles[index].size > 2 * 1024 * 1024 ? 50 : 90,
+            success ({tempFilePath}) {
+              let list = that.data[key].concat([{
+                "path": tempFilePath, "id": null
+              }])
+              that.setData({
+                [key]: list
+              })
+            }
+          })
+        })
+        setTimeout(() => {app.globalData.isIgnoreRefresh = false}, 300)
+      }
+    })
+  },
+  previewImage: function (e) {
+    let key = e.currentTarget.dataset.name
+    app.globalData.isIgnoreRefresh = true
+    wx.previewImage({
+      current: e.currentTarget.id,
+      urls: this.data[key].map(item => {return item.path})
+    })
+  },
+  removeImage (e) {
+    let key = e.currentTarget.dataset.name
+    let index = e.currentTarget.dataset.index;
+    let _this = this
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除吗？',
+      success: function (sm) {
+        if (sm.confirm) {
+          _this.data[key].splice(index, 1)
+          _this.setData({
+            [key]: _this.data[key]
+          })
+          let id = e.currentTarget.dataset.id;
+          if (id) {
+            common.deleteImage(id)
+          }
+        } else if (sm.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  prepareUploadImage () {
+    let _this = this
+    let investigatorImageFiles = []
+    _this.data.investigatorImageFiles.map(item => {
+      if (item.path.indexOf('https://') == -1){
+        investigatorImageFiles.push({path: item.path, type: 2})
+      }
+    })
+
+    return [...investigatorImageFiles]
+  },
+  uploadImage () {
+    let _this = this
+    let imgPaths = this.prepareUploadImage()
+    let count = 0
+    let successUp = 0
+    let failUp = 0
+    if (imgPaths.length) {
+      _this.uploadOneByOne(imgPaths,successUp,failUp,count,imgPaths.length)
+    } else {
+      wx.showToast({
+        mask: true,
+        title: '提交成功',
+        icon: 'success',
+        duration: 1000,
+        success () {
+          _this.goToList()
+        }
+      })
+    }
+  },
+  uploadOneByOne (imgPaths,successUp, failUp, count, length, callback) {
+    let that = this
+    let formData = {
+      'flowId': that.data.orderId,
+      'type': imgPaths[count].type
+    }
+    wx.uploadFile({
+      url: 'https://aplusprice.xyz/aprice/app/image/upload',
+      filePath: imgPaths[count].path,
+      name: `files`,
+      header: {
+        "Content-Type": "multipart/form-data",
+        'token': wx.getStorageSync('token')
+      },
+      formData: formData,
+      success:function(e){
+        let responseCode = JSON.parse(e.data)
+        if (responseCode.code == 0) {
+          successUp++;//成功+1
+        } else {
+          failUp++;//失败+1
+        }
+      },
+      fail:function(e){
+        failUp++;//失败+1
+      },
+      complete:function(e){
+        count++;//下一张
+        if(count == length){
+          console.log('上传成功' + successUp + ',' + '失败' + failUp);
+          if (callback) {
+            callback()
+          }
+          wx.showToast({
+            mask: true,
+            title: length == successUp ? '提交成功' : `图片上传失败:${failUp}`,
+            icon: length == successUp ? 'success' : 'none',
+            duration: 1000,
+            success () {
+              if (length == successUp) {
+                setTimeout(() => {
+                  that.goToList()
+                }, 1000)
+              }
+            }
+          })
+        }else{
+          //递归调用，上传下一张
+          that.uploadOneByOne(imgPaths, successUp, failUp, count, length, callback);
+          console.log('正在上传第' + count + '张');
+        }
+      }
+    })
+  },
 })
