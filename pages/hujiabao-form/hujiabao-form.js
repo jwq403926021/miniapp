3249beab39137f905b5df774888a70bed92834b0
jiangwenqi <jwq403926021@163.com>
2023-08-01 11:09:43 +0800

    #沪家保

--- pages/hujiabao-form/hujiabao-form.js
+++ pages/hujiabao-form/hujiabao-form.js
@@ -8,33 +8,36 @@ Page({
   behaviors: [computedBehavior.behavior],
   computed: {
     isEditable (data) {
-      // 1 查勘员、业主、物业、施工人员、平台处理人、报价人员、财务人员、tis人员、市级负责人、省级负责人
-      // data.role data.status
       let isEditable = true
       return isEditable
     }
   },
   data: {
-    role: 1,
+    role: 1, // 理算 施工人员
     orderId: null,
     statusMap: {
-      '20': '已派送',
-      '13': '已确认',
-      '42': '已中标',
-      '37': '待处理人指派比价人员',
-      '38': '申请退单',
-      '52': '待查勘员审核',
-      '53': '查勘员驳回',
-      '39': '比价中',
-      '51': '待处理人确认',
-      '33': '处理人驳回',
-      '41': '待报价',
-      '43': '报价驳回',
-      '36': '待查勘员处理',
-      '35': '待施工',
-      '50': '已完工待财务处理',
-      '11': '已办结'
-    }
+      '1': '待查勘',
+      '2': '线下已提交',
+      '3': '已查勘',
+      '4': '待理算',
+      '5': '待核损',
+      '6': '核损通过',
+      '7': '已关闭',
+    },
+    delayReportReasonSelectedIndex: '',
+    delayReportReasonValue: '',
+    delayReportReasonList: Object.values(MetaData.delayReportReason),
+    delayReportReasonLabel: ''
+  },
+  pickerChange (e) {
+    let name = e.currentTarget.dataset.name;
+    let source = MetaData[name]
+    let key = Object.keys(source)[e.detail.value]
+    this.setData({
+      [`${name}SelectedIndex`]: e.detail.value,
+      [`${name}Value`]: key,
+      [`${name}Label`]: source[key] ? source[key] : ''
+    })
   },
   async onLoad (routeParams) {
 
